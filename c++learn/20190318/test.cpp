bae114eaeced6d037674ac85ebd361dcd668c9d0
LBC <506458819@qq.com>
2019-03-19 14:13:51 +0800

    class learn and list try

---
+++ c++learn/20190318/test.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class solution{
+public:
+    vector<int> in;
+    int target;
+    vector<int> out;
+    void twosum(){
+        unordered_map<int, int> m;
+        for(int i = 0; i < in.size(); ++i){
+            if(m.count(target-in[i])){
+                out.push_back(i);
+                out.push_back(m[target-in[i]]);
+            }
+            m[in[i]] = i;
+        }
+
+
+    }
+
+
+};
+
+int main(){
+    solution lbc;
+    //init
+    cout << "please input numbers:";
+    int temp;
+    while(cin >> temp){
+        lbc.in.push_back(temp);
+    }
+    cin.clear(); 
+    cin.sync();
+    cout << "please input the number you want to find:";
+    
+    cin >> lbc.target;
+    lbc.twosum();
+    vector<int>::iterator v = lbc.out.begin();
+    while( v != lbc.out.end()) {
+      cout << *v+1 << endl;
+      v++;
+   }
+    return 0;
+}
