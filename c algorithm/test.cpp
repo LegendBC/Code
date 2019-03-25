149ee3cf1e25a5850b930dd4e98950d678935e4f
LBC <506458819@qq.com>
2019-03-25 14:32:12 +0800

    do somethiong

---
+++ c algorithm/test.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int a[100][100];
+int d[100][100];
+
+
+
+int main()
+{
+    int m,n;
+    scanf("%d %d",&m,&n);
+    for(int i =0;i<m;++i)
+    {
+        for(int j = 0;j<n;++j)
+        {
+            scanf("");
+        }
+    }
+    return 0;
+}
---
+++ c++learn/20190318/list.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+struct ListNode {
+      int val;  //当前结点的值
+      ListNode *next;  //指向下一个结点的指针
+      ListNode(int x) : val(x), next(NULL) {}  //初始化当前结点值为x,指针为空
+  };
+
+class solution{
+public:
+    ListNode *addtwonumbers(ListNode *l1, ListNode *l2){
+        ListNode *ret = new ListNode(-1);
+        ListNode *cur = ret;
+        int carry = 0;
+
+    }
+};
+
+int main()
+{
+    
+
+    return 0;
+}
